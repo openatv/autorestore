905276e2b2b4323c1ea8c3302425c5c8faa9f17f
Captain <captain.onboard@web.de>
2023-10-07 07:41:59 +0200

    restart_services start not in background before update package start

--- src/fastrestore_openatv.sh
+++ src/fastrestore_openatv.sh
@@ -279,7 +279,7 @@ spinner $! "Network "
 echo >>$LOG
 
 # Restart certain services and remount media in "turbo" mode ...
-(restart_services) &
+(restart_services)
 
 if [ $plugins -eq 1 ] && [ -e ${ROOTFS}tmp/installed-list.txt ]; then
 	(restore_plugins) &
